13ad1d5c7b9c10792a0221e35de926eb4da4e4c2
Valeriy <talover9@gmail.com>
2023-01-04 14:50:18 +0200

    +

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 // phone mask
 Inputmask("+7 999 999 99 99").mask(document.querySelectorAll(".phone-mask"));
+
+var swiper = new Swiper(".photo-slider", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 10,
+  loop: true,
+});
